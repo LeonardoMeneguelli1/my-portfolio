6cd1b32e2bf290f7748e43112f55a794523b75db
leonardomeneguell1 <leonardomeneguelli@hotmail.com>
2025-02-13 19:41:21 -0300

    feat: creat Navbar componet UI

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,25 +1,41 @@
-import { AppBar, MenuItem, styled, Toolbar } from "@mui/material"
-
+import { AppBar, MenuItem, styled, Toolbar } from "@mui/material";
 
 const Navbar = () => {
+  const StyledToolbar = styled(Toolbar)({
+    display: "flex",
+    justifyContent: "space-evenly",
+  });
 
-    const StyledToolbar = styled(Toolbar)(({theme}) => ({
-        display: 'flex',
-        justifyContent: 'space-evenly',
-      }));
-      
-    return (
-      <>
-        <AppBar position="absolute">
-            <StyledToolbar>
-                <MenuItem>About</MenuItem>
-                <MenuItem>Skills</MenuItem>
-                <MenuItem>Projects</MenuItem>
-            </StyledToolbar>
-        </AppBar>
-      </>
-    )
-  }
-  
-  export default Navbar
-  
+  const StyledMenuItem = styled(MenuItem)(({ theme }) => ({
+    cursor: "pointer",
+    color: "inherit",
+    transition: "0.3s",
+    "&:hover": {
+      color: theme.palette.secondary.light,
+    },
+  }));
+
+  const handleSmoothScroll = (targetId) => {
+    setTimeout(() => {
+      document.querySelector(targetId)?.scrollIntoView({ behavior: "smooth" });
+    },);
+  };
+
+  return (
+    <AppBar position="absolute">
+      <StyledToolbar>
+        <StyledMenuItem onClick={() => handleSmoothScroll("#about")}>
+          About
+        </StyledMenuItem>
+        <StyledMenuItem onClick={() => handleSmoothScroll("#skills")}>
+          Skills
+        </StyledMenuItem>
+        <StyledMenuItem onClick={() => handleSmoothScroll("#projects")}>
+          Projects
+        </StyledMenuItem>
+      </StyledToolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
 import Navbar from "../../components/Navbar/Navbar"
 import About from "./sections/About/About"
 import Hero from "./sections/Hero/Hero"
+import Projects from "./sections/Projects/Projects"
+import Baseboard from "./sections/Baseboard/Baseboard"
 
 const Home = () => {
 
@@ -9,6 +11,8 @@ const Home = () => {
         <Navbar/>
         <Hero/>
         <About/>
+        <Projects/>
+        <Baseboard/>
       </>
     )
   }
